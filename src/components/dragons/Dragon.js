550f39d59225470f4e85eea0d29d8e9f67cd1a11
ramrod433 <djoletoamanor@gmail.com>
2022-06-15 13:47:28 +0000

    Added frtch to store

--- src/components/dragons/Dragon.js
+++ src/components/dragons/Dragon.js
@@ -1,7 +1,32 @@
-const Dragon = () => (
-  <div>
-    Dragons
-  </div>
-);
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDragons } from '../../redux/dragons/Dragons';
+import DragonElement from './DragonElement';
+import './dragons.css';
 
-export default Dragon;
+const Dragons = () => {
+  const dispatch = useDispatch();
+  const dragonsList = useSelector((store) => store.dragons);
+  useEffect(() => {
+    dispatch(getDragons());
+  }, []);
+
+  return (
+    <section>
+      <ul className="dragonsList">
+        {dragonsList.map((dragon) => (
+          <DragonElement
+            key={dragon.dry_mass_kg}
+            id={dragon.id}
+            name={dragon.name}
+            description={dragon.description}
+            flickrImages={dragon.flickr_images[0]}
+            reserved={dragon.reserved}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Dragons;
