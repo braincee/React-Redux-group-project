f83114062a68b67d90cbddf1245c9faf913f6a30
ramrod433 <djoletoamanor@gmail.com>
2022-06-16 17:22:32 +0000

    Added dragons to profile

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -1,8 +1,10 @@
 import MissionProfiles from '../missions/MissionProfiles';
+import Profile from '../dragons/profileDragon';
 
 const MyProfile = () => (
   <div className="grid-display">
     <MissionProfiles />
+    <Profile />
   </div>
 );
 
