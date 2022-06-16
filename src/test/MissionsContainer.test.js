708362381072a7e5ba58bbadd5216c520d55bba0
braincee <kwesi938@gmail.com>
2022-06-16 18:29:46 +0100

    Fix linters

--- src/test/MissionsContainer.test.js
+++ src/test/MissionsContainer.test.js
@@ -3,15 +3,13 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Missions from '../components/missions/MissionsContainer';
 
-
 const MissionList = () => {
-    <Router>
-        <Missions />
-    </Router>
-}
+  <Router>
+    <Missions />
+  </Router>;
+};
 
-test( 'should render missions', () =>
-{
-    const tree = renderer.create( <MissionList /> ).toJSON();
-    expect( tree ).toMatchSnapshot();
-})
+test('should render missions', () => {
+  const tree = renderer.create(<MissionList />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- src/test/Navbar.test.js
+++ src/test/Navbar.test.js
@@ -3,14 +3,13 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from '../components/pages/Navbar';
 
-
 const Header = () => {
-    <Router>
-        <Navbar />
-    </Router>
-}
+  <Router>
+    <Navbar />
+  </Router>;
+};
 
-test( 'should render correctly', () => {
-    const tree = renderer.create( <Header /> ).toJSON();
-    expect( tree ).toMatchSnapshot();
-})
+test('should render correctly', () => {
+  const tree = renderer.create(<Header />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
