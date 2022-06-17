dec7007a49d982d54e4ee14f1187677575aa25c4
ramrod433 <djoletoamanor@gmail.com>
2022-06-17 15:57:13 +0000

    Update profile

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -1,9 +1,11 @@
 import MissionProfiles from '../missions/MissionProfiles';
+
 import Profile from '../dragons/profileDragon';
 
 const MyProfile = () => (
   <div className="grid-display">
     <MissionProfiles />
+    
     <Profile />
   </div>
 );
